82bc50bbb9a5b8f5ecad3295346e4f42bf98afe9
developebyjk <developedbyjk@gmail.com>
2023-09-17 12:42:41 +0530

    changed the content

--- pages/About.jsx
+++ pages/About.jsx
@@ -7,13 +7,30 @@ export default function About() {
         <div className="about-page-container">
             <img src={bgImg} className="about-hero-image" />
             <div className="about-page-content">
-                <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
-                <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉)</p>
-                <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+                <h1>Steps</h1>
+                <ul>
+                    <li>dont forget to add git ignore</li>
+                    <li>
+                        git ignore will have files like npm,etc that we dont wan to uplod 
+                    </li>
+                    <li>
+                    then do This.. git init then git status.. then git add . then git commit -m 'message' then git push
+                    </li>
+                    <li>then it will be uploded to github</li>
+
+<li>this is your react proejct so netlfiy will automatically understand and apply</li>                
+                <li>once you build </li>
+                <li>boom you didi it</li>
+                <li>continously deployment started</li>
+                <li>next time just do git pull then.. git add . then git commit -m 'msg' then git push</li>
+
+
+
+</ul>
             </div>
             <div className="about-page-cta">
-                <h2>Your destination is waiting.<br />Your van is ready.</h2>
-                <Link className="link-button" to="/vans">Explore our vans</Link>
+                <h2>hope you learn something new. checkout our other learnings</h2>
+                <Link className="link-button" to="/vans">I wanna learn!</Link>
             </div>
         </div>
     );
